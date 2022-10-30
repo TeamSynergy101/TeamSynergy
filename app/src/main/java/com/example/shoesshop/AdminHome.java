6524653e6106c9d53e2567e08a8457be1ae90d15
it21027406 <it21027406@my.sliit.lk>
2022-10-30 21:39:01 +0530

    final commit

---
+++ app/src/main/java/com/example/shoesshop/AdminHome.java
@@ -0,0 +1,40 @@
+package com.example.shoesshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class AdminHome extends AppCompatActivity {
+
+    ImageView imageView1;
+    ImageView imageView2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_home);
+
+        imageView1=(ImageView)findViewById(R.id.food);
+        imageView2=(ImageView)findViewById(R.id.addv);
+
+        imageView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AdminHome.this, ManageProducts.class);
+                startActivity(intent);
+            }
+        });
+
+        imageView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AdminHome.this, ChooseActor.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
